5e465db3ba7397b48d262e4df9471d543f299fe5
Amy Dutton <amy@ahhacreative.com>
2021-03-17 12:48:34 -0500

    💄 Styled the Host Section
    - Fixed grid

--- nextjs/src/modules/home/components/TheHosts.js
+++ nextjs/src/modules/home/components/TheHosts.js
@@ -9,7 +9,6 @@ const TheHosts = () => {
       <div className="section-heading__wrapper">
         <h3 className="section-heading">The Hosts</h3>
       </div>
-      {/* <div className="two-columns"> */}
       {/* james */}
       <div className="james">
         <div>
@@ -23,23 +22,20 @@ const TheHosts = () => {
             </div>
           </div>
         </div>
-        <div>
-          <p className="large-body-copy">
-            I'm a Fullstack Web Developer who is addicted to learning and loves working with people.
+        <p className="large-body-copy">
+          I'm a Fullstack Web Developer who is addicted to learning and loves working with people.
           </p>
-          <p className="body-copy">
-            I’m a Developer Advocate at Auth0 and have over 7 years of experience in Advocacy and Software Development. In my spare time, I run a YouTube channel, play co-ed soccer with my wife, spend time with my dogs, and can solve a Rubik's cube in under a minute.
+        <p className="body-copy">
+          I’m a Developer Advocate at Auth0 and have over 7 years of experience in Advocacy and Software Development. In my spare time, I run a YouTube channel, play co-ed soccer with my wife, spend time with my dogs, and can solve a Rubik's cube in under a minute.
           </p>
-          <div className="social-media">
-            <SocialMedia
-              twitter="http://twitter.com/jamesqquick"
-              github="http://github.com/jamesqquick"
-              youtube="http://youtube.com/jamesqquick"
-              twitch="http://twitch.com/jamesqquick"
-            />
-          </div>
+        <div className="social-media">
+          <SocialMedia
+            twitter="http://twitter.com/jamesqquick"
+            github="http://github.com/jamesqquick"
+            youtube="http://youtube.com/jamesqquick"
+            twitch="http://twitch.com/jamesqquick"
+          />
         </div>
-
       </div>
 
       {/* amy */}
@@ -55,24 +51,21 @@ const TheHosts = () => {
             </div>
           </div>
         </div>
-        <div>
-          <p className="large-body-copy">
-            I love️ teaching designers how to code and developers how to design.
-          </p>
-          <p className="body-copy">
-            I'm a Senior UI / UX designer and frontend developer at ZEAL. I have over 20 years of web experience, officially making me a granny in Internet years. I live in Nashville, TN with my husband, 3 adorable kids, and 2 dogs. If I'm not sitting in front of my computer making things, I'm hanging out with family and friends. I love streaming all the things, playing cards, reading, and drinking coffee. Lots of coffee.
-          </p>
-          <div className="social-media">
-            <SocialMedia
-              twitter="http://twitter.com/selfteachme"
-              instagram="http://instagram.com/selfteachme"
-              github="http://github.com/ahaywood"
-              youtube="http://youtube.com/c/selfteachme"
-            />
-          </div>
+        <p className="large-body-copy">
+          I love️ teaching designers how to code and developers how to design.
+            </p>
+        <p className="body-copy">
+          I'm a Senior UI / UX designer and frontend developer at ZEAL. I have over 20 years of web experience, officially making me a granny in Internet years. I live in Nashville, TN with my husband, 3 adorable kids, and 2 dogs. If I'm not sitting in front of my computer making things, I'm hanging out with family and friends. I love streaming all the things, playing cards, reading, and drinking coffee. Lots of coffee.
+            </p>
+        <div className="social-media">
+          <SocialMedia
+            twitter="http://twitter.com/selfteachme"
+            instagram="http://instagram.com/selfteachme"
+            github="http://github.com/ahaywood"
+            youtube="http://youtube.com/c/selfteachme"
+          />
         </div>
       </div>
-      {/* </div> */}
     </StyledTheHosts>
   )
 }
@@ -80,20 +73,43 @@ const TheHosts = () => {
 const StyledTheHosts = styled.section`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 0;
+  margin: 0 auto 60px;
+  max-width: 1300px;
+  width: 100%;
+
+  .james {
+    border-right: 1px solid ${props => props.theme.white};
+    padding-right: 85px;
+  }
+
+  .amy {
+    padding-left: 85px;
+  }
+
+  .james,
+  .amy {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
 
   .section-heading__wrapper {
     grid-column: span 2;
+    margin-bottom: 60px;
     text-align: center;
     width: 100%;
   }
 
   .meta {
     display: flex;
+    padding-top: 60px;
     width: 100%;
   }
 
   .avatar {
     margin-right: 20px;
+    width: 187px;
   }
 
   /* name */
@@ -132,6 +148,10 @@ const StyledTheHosts = styled.section`
   }
 
   .social-media {
+    margin: auto auto 0 0;
+    justify-self: flex-end;
+    padding-top: 10px;
+    padding-bottom: 60px;
 
     ul {
       justify-content: flex-start;
