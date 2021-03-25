ab8471ed6168ed4f4be435c2697ce3b4d9505551
Amy Dutton <amy@ahhacreative.com>
2021-03-24 23:32:52 -0500

    ✨ Stubbed out the results page

---
+++ nextjs/src/pages/results.js
@@ -0,0 +1,10 @@
+import { InteriorLayout } from "modules/shared/layouts/InteriorLayout";
+// import { SponsorApplicationPage } from "modules/sponsorApplication";
+
+export default function Results() {
+  return (
+    <InteriorLayout>
+      Search Results
+    </InteriorLayout>
+  )
+}
