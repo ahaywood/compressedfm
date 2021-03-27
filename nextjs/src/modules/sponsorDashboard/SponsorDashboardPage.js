f2715ed8535e2a54a53b76124ef472a807212b42
Amy Dutton <amy@ahhacreative.com>
2021-03-27 15:04:52 -0500

    ✨ Stubbed out Sponsor Dashbboard

--- nextjs/src/modules/sponsorDashboard/SponsorDashboardPage.js
+++ nextjs/src/modules/sponsorDashboard/SponsorDashboardPage.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
 import { useState } from "react";
 import styled from "styled-components";
 import { SponsorAudioPlayer } from "modules/shared/components/AudioPlayer/SponsorAudioPlayer";
 import { FeaturedAudioPlayer } from "modules/shared/components/AudioPlayer/FeaturedAudioPlayer";
+import { Icon } from "modules/shared/components/Icon";
+import { VerticalDivider } from "modules/shared/components/VerticalDivider";
+import { MixinHeading } from "styles/Typography"
 
 const SponsorDashboardPage = () => {
   const [currentlyPlaying, setCurrentlyPlaying] = useState();
@@ -12,6 +16,76 @@ const SponsorDashboardPage = () => {
 
   return (
     <StyledSponsorDashboardPage>
+      <h1>Thanks for Sponsoring!</h1>
+      <img src="" />
+
+      <div className="box">
+        <h3 className="number">30,489</h3>
+        <h5>Total Listens</h5>
+      </div>
+
+      <div className="box">
+        <h3 className="number">32,520</h3>
+        <h5>Total Downloads</h5>
+      </div>
+
+      <div>
+        <Link href="/sponsor-application">
+          <a className="button">Sponsor Again</a>
+        </Link>
+      </div>
+
+
+      <VerticalDivider />
+
+
+      {/* contracts & invoices */}
+      <div className="contracts-invoices">
+        <div className="contracts-invoices__header">Contracts</div>
+        <div className="space"></div>
+        <div className="contracts-invoices__header">Invoices</div>
+
+        {/* contract */}
+        <div className="contract-status"><span>Attn</span></div>
+        <div className="contract-date">02.28.21</div>
+        <div className="contract-quantity">3</div>
+        <div className="contract-description">Bundle</div>
+        <div className="contract-arrow">
+          <Icon name="arrow" />
+        </div>
+
+        {/* invoice */}
+        <div className="invoice-status"><span>Attn</span></div>
+        <div className="invoice-number">#0093</div>
+        <div className="invoice-description">3 EPISODES: 60 seconds</div>
+        <div className="invoice amount">$150</div>
+        <div className="invoice-arrow"><Icon name="arrow" /></div>
+
+
+
+        {/* contract */}
+        <div className="contract-status"><span>Attn</span></div>
+        <div className="contract-date">02.28.21</div>
+        <div className="contract-quantity">3</div>
+        <div className="contract-description">Bundle</div>
+        <div className="contract-arrow">
+          <Icon name="arrow" />
+        </div>
+
+        {/* invoice */}
+        <div className="invoice-status"><span>Attn</span></div>
+        <div className="invoice-number">#0093</div>
+        <div className="invoice-description">3 EPISODES: 60 seconds</div>
+        <div className="invoice amount">$150</div>
+        <div className="invoice-arrow"><Icon name="arrow" /></div>
+      </div>
+
+      <VerticalDivider />
+
+
+      <h2 className="heading">Episodes You've Sponsored</h2>
+
+      {/* PLAYERS */}
       <SponsorAudioPlayer id="1" currentlyPlaying={currentlyPlaying} handleMultipleAudioPlayers={handleMultipleAudioPlayers} />
       <SponsorAudioPlayer id="2" currentlyPlaying={currentlyPlaying} handleMultipleAudioPlayers={handleMultipleAudioPlayers} />
       <SponsorAudioPlayer id="3" currentlyPlaying={currentlyPlaying} handleMultipleAudioPlayers={handleMultipleAudioPlayers} />
@@ -20,7 +94,9 @@ const SponsorDashboardPage = () => {
 }
 
 const StyledSponsorDashboardPage = styled.section`
-
+  .heading {
+    ${MixinHeading}
+  }
 `;
 
 export { SponsorDashboardPage }
