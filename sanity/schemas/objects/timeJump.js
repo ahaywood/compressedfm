b2df5d366eca63fbf692744826eb4085f89cfdde
Amy Dutton <amy@ahhacreative.com>
2021-04-13 01:59:10 -0500

    ✨ Updated the individual episode page

--- sanity/schemas/objects/timeJump.js
+++ sanity/schemas/objects/timeJump.js
@@ -17,8 +17,8 @@ export default {
     {
       title: 'Time',
       name: 'time',
-      type: 'string',
-      description: 'mm:ss'
+      type: 'number',
+      description: 'in seconds'
     },
     {
       title: 'Description',
