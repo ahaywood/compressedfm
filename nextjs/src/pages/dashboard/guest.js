657c995c24ce2faec4f3ad28dd93acd5ee6377fe
James Quick <james.q.quick@outlook.com>
2021-04-14 09:47:44 -0500

    Protecting guest page same as sponsor page

--- nextjs/src/pages/dashboard/guest.js
+++ nextjs/src/pages/dashboard/guest.js
@@ -1,12 +1,26 @@
-import { useUser } from '@auth0/nextjs-auth0';
-import Link from "next/link";
+import { useUser, withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
+import Custom403 from 'pages/403';
+import client from 'utils/client';
+import { guestQuery } from 'utils/queries';
 
-export default function guest() {
-  const { user, error, isLoading } = useUser();
+export default function guest({ guest }) {
+    const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-  if (!user) return <Link href="/api/auth/login"><a>Login</a></Link>;
-  return <div>Hello {user.nickname}</div>
+    if (!guest) {
+        return <Custom403 />;
+    }
 
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+    return <div>Hello {user.nickname}</div>;
 }
+
+export const getServerSideProps = withPageAuthRequired({
+    async getServerSideProps({ req, res }) {
+        const { user } = getSession(req, res);
+        const { email } = user;
+        const guest = await client.fetch(guestQuery, { email });
+        return { props: { guest, user } };
+    },
+});
