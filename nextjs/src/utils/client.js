fa83b5e38dbdc1bdaf02b348923a12537c2db60b
Amy Dutton <amy@ahhacreative.com>
2021-04-15 02:52:58 -0500

    🚨 Added API Version

--- nextjs/src/utils/client.js
+++ nextjs/src/utils/client.js
@@ -4,5 +4,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'gqnsvyvh', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
+  apiVersion: '2021-03-25', // use a UTC date string
   useCdn: false // `false` if you want to ensure fresh data
 })
