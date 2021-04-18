c52527588c32275fcc7fa70cb5fcc21263dbcff5
Amy Dutton <amy@ahhacreative.com>
2021-04-18 08:28:47 -0500

    ✨ Set up Custom Time Input field

--- sanity/schemas/components/timeInput.js
+++ sanity/schemas/components/timeInput.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { withDocument } from 'part:@sanity/form-builder'
-
+import React, { useState, useEffect, useRef } from 'react'
 import FormField from 'part:@sanity/components/formfields/default'
 import { Box, TextInput, Flex, Label } from '@sanity/ui'
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event'
 
-function createPatchFrom(value) {
-  return PatchEvent.from(value === '' ? unset() : set(value))
+// sanity's patch event
+const createPatchFrom = (value) => {
+  return PatchEvent.from(value === '' ? unset() : set(value));
 }
 
 // split out seconds into hours, minutes, and seconds
@@ -15,8 +13,8 @@ const incomingValue = (seconds) => {
   // determine how many minutes
   let hours = 0;
   let remainingMinutes = 0;
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.floor(seconds % 60);
+  const minutes = Math.floor(Number(seconds) / 60);
+  const remainingSeconds = Math.floor(Number(seconds) % 60);
 
   // are there hours?
   if (minutes > 59) {
@@ -25,72 +23,71 @@ const incomingValue = (seconds) => {
   }
 
   return {
-    hours,
-    minutes: remainingMinutes ? remainingMinutes : minutes,
-    seconds: remainingSeconds
+    hours: leadingZeros(hours),
+    minutes: leadingZeros(remainingMinutes ? remainingMinutes : minutes),
+    seconds: leadingZeros(remainingSeconds)
   }
+}
 
+const leadingZeros = (number) => {
+  if (number < 10) {
+    return `0${number}`;
+  } else if (number < 1) {
+    return "00";
+  }
+  return number;
 }
 
 // convert time to seconds
 const outgoingValue = (hours, minutes, seconds) => {
-  const hoursInSeconds = hours * 60 * 60;
-  const minutesInSeconds = minutes * 60;
-  return hoursInSeconds + minutesInSeconds + seconds;
+  const hoursInSeconds = Number(hours) * 60 * 60;
+  const minutesInSeconds = Number(minutes) * 60;
+  const convertedTime = hoursInSeconds + minutesInSeconds + Number(seconds);
+  return convertedTime;
 }
 
-const TimeInput = ({ type, value, onChange, inputComponent }) => {
-  const [inSeconds, setInSeconds] = useState();
+/** -------------------------------------------------
+* COMPONENT
+---------------------------------------------------- */
+const TimeInput = React.forwardRef(({ type, value, onChange }, inputComponent) => {
   const { hours, minutes, seconds } = incomingValue(value);
+
+  focus = () => {
+    inputComponent.current.focus()
+  }
+
+
+  const handleTimeChange = (e) => {
+    const time = e.target.value.split(':');
+
+    let newVal;
+
+    // hh:mm:ss
+    if (time.length > 1) {
+      newVal = outgoingValue(time[0], time[1], time[2]);
+    } // mm:ss
+    else if (time.length > 0) {
+      newVal = outgoingValue(0, time[0], time[1]);
+    } // ss
+    else {
+      newVal = time[0]
+    }
+
+    onChange(createPatchFrom(String(newVal)))
+  }
 
   return (
     <div>
       <FormField label={type.title} description={type.description} />
-      <Flex>
-        <Box>
-          <Label size={0} style={{ marginBottom: "5px" }}>Hours</Label>
-          <TextInput
-            type="text"
-            // ref={ref}
-            placeholder="hh"
-            value={hours}
-            defaultValue={hours}
-            onChange={event => { onChange(PatchEvent.from(set(event.target.value))) }}
-          />
-        </Box>
-        <Box>:</Box>
-        <Box>
-          <Label size={0} style={{ marginBottom: "5px" }}>Minutes</Label>
-          <TextInput
-            type="text"
-            // ref={ref}
-            placeholder="mm"
-            value={minutes}
-            defaultValue={minutes}
-            onChange={event => { onChange(PatchEvent.from(set(event.target.value))) }}
-          />
-        </Box>
-        <Box>:</Box>
-        <Box>
-          <Label size={0} style={{ marginBottom: "5px" }}>Seconds</Label>
-          <TextInput
-            type="text"
-            // ref={ref}
-            placeholder="ss"
-            value={seconds}
-            defaultValue={seconds}
-            onChange={event => { onChange(PatchEvent.from(set(event.target.value))) }}
-          />
-        </Box>
-      </Flex>
-      <input
-        type="hidden"
-        value={inSeconds}
+      <TextInput
+        type="text"
         ref={inputComponent}
-        onChange={event => { onChange(PatchEvent.from(set(event.target.value))) }}
+        defaultValue={`${hours}:${minutes}:${seconds}`}
+        onChange={e => handleTimeChange(e)}
+        placeholder="hh:mm:ss"
       />
     </div>
   )
-}
+});
 
-export default TimeInput
+export default TimeInput;
