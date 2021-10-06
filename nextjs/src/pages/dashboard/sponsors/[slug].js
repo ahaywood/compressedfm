c86dbba8908e34c95dfae178de1790e322233526
James Quick <james.q.quick@outlook.com>
2021-10-06 12:06:13 -0500

    Changed sponsor dashboard pages to be dynamic route based on the slug of the sponsor. Sponsor model now also includes a slug property. New environment variable, ADMIN_EMAILS, which is a comma separated list of emails that should have access to all dashboard pages

--- nextjs/src/pages/dashboard/sponsors/[slug].js
+++ nextjs/src/pages/dashboard/sponsors/[slug].js
@@ -3,9 +3,9 @@ import { InteriorLayout } from 'modules/shared/layouts/InteriorLayout';
 import { SponsorDashboardPage } from 'modules/sponsorDashboard';
 import { useUser, getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 
-import { sponsorQuery } from 'utils/queries';
+import { sponsorBySlugQuery, sponsorQuery } from 'utils/queries';
 import { getStatsForEpisodes } from 'utils/simpleCast';
-import CustomError from '../customError';
+import CustomError from '../../customError';
 
 export default function sponsor({ sponsor, error = null }) {
   if (error) {
@@ -22,11 +22,16 @@ export default function sponsor({ sponsor, error = null }) {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  async getServerSideProps({ req, res }) {
+  async getServerSideProps(context) {
+    const { req, res } = context;
+    const { slug = '' } = context.query;
     const { user } = getSession(req, res);
     const { email } = user;
-    const sponsor = await client.fetch(sponsorQuery, { email });
     try {
+      const sponsor = await client.fetch(sponsorBySlugQuery, { slug });
+      if(!sponsor.associatedEmails?.includes(email) && !process.env.ADMIN_EMAILS.includes(email)){
+        return {props: {sponsor: null}};
+      }
       const episodeIds = sponsor.episodes.map((episode) => episode.simplecastId);
       const stats = await getStatsForEpisodes(episodeIds);
       for (let i = 0; i < sponsor.episodes.length; i++) {
--- nextjs/src/utils/queries.js
+++ nextjs/src/utils/queries.js
@@ -20,6 +20,27 @@ export const sponsorQuery = groq`*[_type == "sponsor" && associatedEmails match 
   }
 }[0]`;
 
+export const sponsorBySlugQuery = groq`*[_type == "sponsor" && slug.current == $slug && published==true]{
+  title,
+  "logo": logo.asset->url,
+  offer,
+  offerLink,
+  offerLink,
+  about,
+  founding,
+  associatedEmails,
+  contractsInvoices[]{
+    ...,
+    "contractPDF": contractPDF.asset->url,
+    "invoicePDF": invoicePDF.asset->url
+  },
+  "episodes": *[_type=='episode' && references(^._id)]{
+    title,
+    audioPath,
+    simplecastId
+  }
+}[0]`;
+
 export const guestQuery = groq`*[_type == "guest" && guestEmail match $email && published==true] {
   firstName,
   lastName,
