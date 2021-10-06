2161ef769224dbb297e868dfad1d9704e608ade2
Amy Dutton <amy@ahhacreative.com>
2021-10-05 23:47:00 -0500

    ✨ Set up page to render on the server

--- nextjs/src/pages/episode/[slug].js
+++ nextjs/src/pages/episode/[slug].js
@@ -1,9 +1,14 @@
 import client from 'utils/client';
 import groq from 'groq';
+
+// components
 import { InteriorLayout } from 'modules/shared/layouts/InteriorLayout';
 import { IndividualEpisodePage } from 'modules/episodes/IndividualEpisodePage';
 
-export default function Episode({episode}) {
+// queries
+import { AllEpisodesQuery } from '../episodes';
+
+export default function Episode({ episode }) {
   return (
     <InteriorLayout>
       <IndividualEpisodePage episode={episode} />
@@ -11,7 +16,7 @@ export default function Episode({episode}) {
   );
 }
 
-const query = groq`*[_type == "episode" && slug.current == $slug] {
+const IndividualEpisodeQuery = groq`*[_type == "episode" && slug.current == $slug] {
   _id,
   audioPath,
   briefDescription,
@@ -56,8 +61,26 @@ const query = groq`*[_type == "episode" && slug.current == $slug] {
   }
 }[0]`;
 
-export async function getServerSideProps(context) {
-  const { slug = '' } = context.query;
-  const episode = await client.fetch(query, { slug });
-  return {props: {episode}}
+export async function getStaticPaths() {
+  const allEpisodes = await client.fetch(AllEpisodesQuery);
+
+  // Get the paths we want to pre-render based on episodes
+  const paths = allEpisodes.map((episode) => ({
+    params: { slug: episode.slug.current },
+  }))
+
+  return { paths, fallback: false }
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const episode = await client.fetch(IndividualEpisodeQuery, { slug });
+  return {
+    props: {
+      episode
+    },
+  }
 }
