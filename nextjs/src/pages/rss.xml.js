c7b341ca27e71577d81a53be15a952366513c2b8
James Q Quick <james.q.quick@gmail.com>
2022-01-07 12:25:43 -0600

    Updated link again

--- nextjs/src/pages/rss.xml.js
+++ nextjs/src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import client from 'utils/client';
 import { AllEpisodesQuery } from 'utils/queries';
 
-const website = 'https://compressed.fm/';
+const website = 'https://compressed.fm';
 
 const xml = (episodes) =>
   `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
