659dbf9f3cbcfecdc71ede1c737259cf9cbc74c2
Amy Dutton <amy@ahhacreative.com>
2022-03-16 19:01:18 -0500

    🚧 Updates to the About page

--- nextjs/src/modules/about/AboutPage.js
+++ nextjs/src/modules/about/AboutPage.js
@@ -9,6 +9,7 @@ import { EpisodeGrid } from 'modules/shared/components/EpisodeGrid';
 import { HorizontalDivider } from 'modules/shared/components/HorizontalDivider';
 import { Faq } from './components/Faq';
 import { ColophonColor } from './components/ColophonColor';
+import { TypeSample } from './components/TypeSample';
 
 const AboutPage = ({ faqs, gettingStarted, mostPopular, siteSettings }) => (
   <StyledAboutPage>
@@ -44,12 +45,13 @@ const AboutPage = ({ faqs, gettingStarted, mostPopular, siteSettings }) => (
       <div className="section-heading__wrapper">
         <h2 className="section-heading">Colophon</h2>
         <p className="large-body">Since we're web designers and developers, we care about this sort of thing.</p>
-        <p>The website is built on Next.js and Sanity and hosted on Vercel.</p>
+        <p>The website is built on <a href="https://nextjs.org/" target="_blank" rel="noreferrer">Next.js</a> and <a href="https://sanity.io/" target="_blank" rel="noreferrer">Sanity</a> and hosted on <a href="https://vercel.com/" target="_blank" rel="noreferrer">Vercel</a>.</p>
         <div className="logo-line">
           <Image src="/images/logo__nextjs@2x.png" alt="Next.js" width="182" height="114" />
           <Image src="/images/logo__sanity@2x.png" alt="Sanity" width="191" height="38" />
           <Image src="/images/logo__vercel@2x.png" alt="Vercel" width="240" height="60" />
         </div>
+        <h4>Colors</h4>
         <div className="colors">
           <ColophonColor color="#ffffff" />
           <ColophonColor color="#000000" border="#444444" />
@@ -57,16 +59,47 @@ const AboutPage = ({ faqs, gettingStarted, mostPopular, siteSettings }) => (
           <ColophonColor color="#FAFF00" />
           <ColophonColor color="#8C59FF" />
         </div>
+        <h4>Typography</h4>
+        <div className="typography">
+          <div className="greyclif">
+            <h5>Greyclif</h5>
+            <TypeSample />
+          </div>
+          <div className="dank-mono">
+            <h5>Dank Mono</h5>
+            <TypeSample />
+          </div>
+        </div>
       </div>
-      <h4>Colors</h4>
-      <h4>Typography</h4>
-      <HorizontalDivider />
-      <div className="large-body">Want to get More specific?</div>
+
+      <HorizontalDivider length="short" />
+
+      <div className="large-body">Want to get more specific?</div>
       <h4>On the Design Side</h4>
-      <div>
-        <p>Amy wrote a blog post on Medium, all about the branding and site design.</p>
-        {/* <Image /> */}
+      <div className="two-up">
+        <div><p>Amy wrote a blog post on Medium, all about the branding and site design.</p></div>
+        <div>{/* <Image /> */}</div>
       </div>
+
+      <br />
+
+      <h4>Want to get even more specific?</h4>
+      <div className="two-up">
+        <div>
+          <div className="large-body">We knew you were our people.</div>
+          <p>We’ve open sourced the code and made it available on GitHub. </p>
+        </div>
+        <div>
+          {/* <Image /> */}
+        </div>
+      </div>
+
+      <br />
+
+      <h4>Still curious?</h4>
+      <p>We took an entire episode to talk about the tech behind the site.</p>
+
+      {/* Audio Player */}
     </section>
     <VerticalDivider />
 
@@ -162,9 +195,66 @@ const StyledAboutPage = styled.div`
 
     }
 
+    .large-body {
+      ${MixinLargeBodyCopy}
+    }
+
     p {
       ${MixinBodyCopy};
       text-align: left;
+
+      a {
+        color: ${props => props.theme.yellow};
+        font-weight: bold;
+        text-decoration: underline;
+
+        &:hover {
+          color: ${props => props.theme.white};
+        }
+      }
+    }
+
+    .logo-line {
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      margin: 40px 0 100px 0;
+    }
+
+    h4 {
+      font-family: ${props => props.theme.mono};
+      color: ${props => props.theme.yellow};
+      text-transform: uppercase;
+      font-size: 1.8rem;
+      text-align: left;
+    }
+
+    .colors {
+      margin-bottom: 65px;
+    }
+
+    .typography {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      text-align: left;
+      font-size: 2.2rem;
+      line-height: 1.2;
+
+      h5 {
+        font-size: 3.6rem;
+        line-height: 1.2;
+        margin: 0 0 10px;
+        padding: 0;
+      }
+
+      .greyclif {
+        font-family: ${props => props.theme.sansSerif};
+        font-weight: bold;
+      }
+
+      .dank-mono {
+        font-family: ${props => props.theme.mono};
+      }
     }
   }
 
