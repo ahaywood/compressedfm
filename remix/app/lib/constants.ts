fd08fae62119b5d408d32d4c797f081c626d5991
Amy Haywood Dutton <me@amyhaywood.com>
2024-01-22 09:35:43 -0600

    ✨ Added Bekah + Brad's info to Constants

--- remix/app/lib/constants.ts
+++ remix/app/lib/constants.ts
@@ -12,6 +12,8 @@ const Constants = Object.freeze({
   COMPRESSEDFM_INSTAGRAM_URL: 'https://instagram.com/compressedfm',
   COMPRESSEDFM_TWITTER_URL: 'https://twitter.com/compressedfm',
   COMPRESSEDFM_RSS: 'https://www.compressed.fm/rss.xml',
+  COMPRESSEDFM_TWITCH: 'https://www.twitch.tv/compressedfm',
+  COMPRESSEDFM_YOUTUBE: 'https://www.youtube.com/compressedfm',
 
   // james
   JAMES_WEBSITE_URL: 'https://jamesqquick.com',
@@ -25,8 +27,23 @@ const Constants = Object.freeze({
   AMY_TWITTER_URL: 'https://twitter.com/selfteachme',
   AMY_GITHUB_URL: 'https://github.com/ahaywood',
   AMY_CODEPEN_URL: 'https://github.com/ahaywood',
-  AMY_INSTAGRAM_URL: 'https://instagram.com/selfteachme',
   AMY_YOUTUBE_URL: 'https://youtube.com/selfteachme',
+  AMY_LINKEDIN_URL: 'https://linkedin.com/in/amy-dutton',
+
+  // brad
+  BRAD_WEBSITE_URL: 'https://bradgarropy.com',
+  BRAD_TWITTER_URL: 'https://twitter.com/bradgarropy',
+  BRAD_GITHUB_URL: 'https://github.com/bradgarropy',
+  BRAD_TWITCH_URL: 'https://www.twitch.tv/bradgarropy',
+  BRAD_YOUTUBE_URL: 'https://www.youtube.com/bradgarropy',
+
+  // bekah
+  BEKAH_WEBSITE_URL: 'https://bekahhw.com/',
+  BEKAH_GITHUB_URL: 'https://github.com/BekahHW',
+  BEKAH_TWITTER_URL: 'https://twitter.com/BekahHW',
+  BEKAH_LINKEDIN_URL: 'https://www.linkedin.com/in/bekah-hawrot-weigel/',
+
+
 });
 
 export { Constants };
