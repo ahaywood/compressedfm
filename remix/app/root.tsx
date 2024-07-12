f7415c3e863a249e096063fd0c004a2c38eaf96c
Amy Haywood Dutton <me@amyhaywood.com>
2024-07-12 07:16:19 -0500

    🐛 Trying to fix Fathom Analytics

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -1,23 +1,15 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "@remix-run/react";
-import stylesheet from "~/tailwind.css";
-import { LiveNow } from "./components/LiveNow";
-import { getClient } from "./lib/sanity";
-import { CurrentlyRecordingQuery } from "./queries/Queries";
-import Fathom from "./components/Fathom";
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
+import stylesheet from '~/tailwind.css';
+import { LiveNow } from './components/LiveNow';
+import { getClient } from './lib/sanity';
+import { CurrentlyRecordingQuery } from './queries/Queries';
+import Fathom from './components/Fathom';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  { rel: "stylesheet", href: stylesheet },
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: stylesheet },
 ];
 
 export const loader = async () => {
@@ -36,12 +28,10 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <script src="https://cdn.usefathom.com/script.js" data-site="TRUYKXEJ" defer></script>
       </head>
       <body>
-        <Fathom />
-        {currentlyRecording ? (
-          <LiveNow youtube={recordingOnYouTube} twitch={recordingOnTwitch} />
-        ) : null}
+        {currentlyRecording ? <LiveNow youtube={recordingOnYouTube} twitch={recordingOnTwitch} /> : null}
         <Outlet />
         <ScrollRestoration />
         <Scripts />
